4f22d58b5038d8cca506c4decf5b43aa3add3f2a
imkostin <63290140+imkostin@users.noreply.github.com>
2022-01-20 13:02:24 +0300

    обновлена модель

--- src/main/java/com/example/formconfigurationservice/Models/Employee.java
+++ src/main/java/com/example/formconfigurationservice/Models/Employee.java
@@ -1,12 +1,11 @@
 package com.example.formconfigurationservice.Models;
 
-import com.sun.tools.javac.util.List;
+import com.example.formconfigurationservice.Enum.Field;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.lang.reflect.Field;
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 @NoArgsConstructor
@@ -16,39 +15,54 @@ public class Employee {
     @Id
     private String id;
     private String formName;
+    private String label;
     private Map<String, String> sattelite; // убогий словарь
-    private List<Field> field;             // убогий список
+    private List<Field> fields;            // убогий список
 
+    public List<Field> getField() {
+        return fields;
+    }
+    public void setField(List<Field> fields) {
+        this.fields = fields;
+    }
 
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+    public void setLabel(String label) {
+        this.label = label;
     }
 
     public String getFormName(String formName) {
         return this.formName;
     }
-
     public void setFormName(String formName) {
-        this.formName = formName;
-    }
-
-    public Employee(String formName) {
         this.formName = formName;
     }
 
     public Map<String, String> getSattelite() {
         return sattelite;
     }
+    public void setSattelite(Map<String, String> sattelite) {
+        this.sattelite = sattelite;
+    }
 
-    public Employee(String id, String formName, List<Field> field, Map<String, String> sattelite) {
+    public Employee(String formName) {
+        this.formName = formName;
+    }
+
+    public Employee(String id, String formName, String label, Map<String, String> sattelite) {
         this.id = null;
         this.formName = formName;
+        this.label = label;
         this.sattelite = sattelite;
-        this.field = field;
     }
 
 }
