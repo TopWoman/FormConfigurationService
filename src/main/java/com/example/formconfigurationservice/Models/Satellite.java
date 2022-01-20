be45b4229df7609b246e59f8429f756821cf7613
imkostin <63290140+imkostin@users.noreply.github.com>
2022-01-20 17:44:45 +0300

    commit

--- src/main/java/com/example/formconfigurationservice/Models/Satellite.java
+++ src/main/java/com/example/formconfigurationservice/Models/Satellite.java
@@ -1,4 +1,15 @@
 package com.example.formconfigurationservice.Models;
 
+import java.util.Map;
+
 public class Satellite {
+
+    private Map<String, String> satelliteType;
+    private Map<String, String> satelliteClassifier;
+
+    public Map<String, String> getSatelliteType() { return satelliteType; }
+    public void setSatelliteType(Map<String, String> satelliteType) { this.satelliteType = satelliteType; }
+
+    public Map<String, String> getSatelliteClassifier() { return satelliteClassifier; }
+    public void setSatelliteClassifier(Map<String, String> satelliteClassifier) { this.satelliteClassifier = satelliteClassifier; }
 }
