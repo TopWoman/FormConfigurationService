f96d2c0c204bfa84ada437f5c00caad08840e13e
imkostin <63290140+imkostin@users.noreply.github.com>
2022-01-21 15:43:05 +0300

    commit

--- src/main/java/com/example/formconfigurationservice/Repository/EmployeeRepository.java
+++ src/main/java/com/example/formconfigurationservice/Repository/EmployeeRepository.java
@@ -1,19 +1,20 @@
 package com.example.formconfigurationservice.Repository;
 
 import com.example.formconfigurationservice.Models.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
-
-//    List<Employee> findByFullNameLike(String fullName);
-
+//      @Query("{formName:'?0'}")
+//      List<Employee> formNameSearch(String fullName);
 //    @Query("{fullName:'?0'}")
-//    List<Employee> findCustomByFullName(String fullName);
 //    List<Employee> satellite(String fullSatellite);
-//
 //    List<Employee> fields(String fullFields);
 
 }
